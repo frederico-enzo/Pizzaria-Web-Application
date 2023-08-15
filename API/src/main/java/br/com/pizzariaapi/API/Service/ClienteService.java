16c251467e696832f7561fd1d4f3e197c0cf2a0b
Frederico <portifolio.frederico@gmail.com>
2023-08-15 17:23:30 -0300

    add dependencia de modelMapper pra conversao de DTO pra entity

--- API/src/main/java/br/com/pizzariaapi/API/Service/ClienteService.java
+++ API/src/main/java/br/com/pizzariaapi/API/Service/ClienteService.java
@@ -1,4 +1,42 @@
 package br.com.pizzariaapi.API.Service;
 
+import br.com.pizzariaapi.API.DTO.ClienteDTO;
+import br.com.pizzariaapi.API.Entity.ClienteEntity;
+import br.com.pizzariaapi.API.Repository.ClienteRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
 public class ClienteService {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+    @Autowired
+    private  ModelMapper modelMapper;
+
+    @Transactional(rollbackFor = Exception.class)
+    public ClienteEntity findById(Long Id){
+        return null;
+    }
+    @Transactional(rollbackFor = Exception.class)
+    public ClienteEntity create(ClienteDTO clienteDTO){
+        return null;
+    }
+    @Transactional(rollbackFor = Exception.class)
+    public ClienteEntity update(ClienteDTO clienteDTO){
+        return null;
+    }
+    @Transactional(rollbackFor = Exception.class)
+    public void delete(Long Id){
+    }
+
+
+
 }
+//        public Produto criarProduto(ProdutoDTO produtoDTO) {
+//            Produto produto = modelMapper.map(produtoDTO, Produto.class);
+//            // Realize qualquer lógica adicional se necessário
+//            return produto;
+//        }
