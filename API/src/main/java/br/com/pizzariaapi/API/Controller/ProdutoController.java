75d47f90b4d5033948e170787537128d07177ef8
Frederico Enzo <portifolio.frederico@gmail.com>
2023-09-12 21:29:28 -0300

    Controller Produto Padronizado

--- API/src/main/java/br/com/pizzariaapi/API/Controller/ProdutoController.java
+++ API/src/main/java/br/com/pizzariaapi/API/Controller/ProdutoController.java
@@ -1,10 +1,12 @@
 package br.com.pizzariaapi.API.Controller;
 
 import br.com.pizzariaapi.API.DTO.AtributoDTO;
+import br.com.pizzariaapi.API.DTO.ClienteDTO;
 import br.com.pizzariaapi.API.DTO.ProdutoDTO;
 import br.com.pizzariaapi.API.Entity.Atributo;
 import br.com.pizzariaapi.API.Entity.Produto;
 import br.com.pizzariaapi.API.Service.AtributoService;
+import br.com.pizzariaapi.API.Service.ClienteService;
 import br.com.pizzariaapi.API.Service.ProdutoService;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,44 +19,40 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/produto")
 public class ProdutoController {
     @Autowired
-    private ProdutoService produtoService;
+    private ProdutoService service;
     @GetMapping(params = "id")
-    public ResponseEntity<?> findById(@RequestParam("id") final Long id){
-        Produto produto = produtoService.findById(id);
-        return produto == null
-                ? ResponseEntity.badRequest().body("Registro  não encontrado")
-                : ResponseEntity.ok(produto);
+    public ResponseEntity<?>findById(@RequestParam("id") final Long id) {
+        try {
+            return ResponseEntity.ok(service.findById(id));
+        }catch(Exception e){
+            return ResponseEntity.badRequest().body("Error : " + e.getMessage());
+        }
     }
     @PostMapping
-    public ResponseEntity<?> create(@RequestBody final ProdutoDTO produtoDTO){
-        try{
-            this.produtoService.create(produtoDTO);
-            return ResponseEntity.ok("Registro cadastrado com sucesso");
-        }catch (EntityNotFoundException e) {
-            return ResponseEntity.notFound().build();
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+    public ResponseEntity<String> create(@RequestBody final ProdutoDTO DTO) {
+        try {
+            return ResponseEntity.ok(service.create(DTO));
+        }catch(Exception e){
+            return ResponseEntity.badRequest().body("Error : " + e.getMessage());
         }
     }
     @PutMapping(params = "id")
-    public ResponseEntity<?> update(@RequestParam("id") final Long id, @RequestBody final ProdutoDTO produtoDTO){
-        try{
-            this.produtoService.update(id, produtoDTO);
-            return ResponseEntity.ok("Registro editado com sucesso");
-        } catch(Exception e){
-            return ResponseEntity.badRequest().body(e.getMessage());
+    public ResponseEntity<String> update(@RequestParam("id") final Long id, @RequestBody final ProdutoDTO DTO){
+        try {
+            return ResponseEntity.ok(service.update(id, DTO));
+        }catch(Exception e){
+            return ResponseEntity.badRequest().body("Error : " + e.getMessage());
         }
     }
     @DeleteMapping(params = "id")
-    public ResponseEntity<?> delete(@RequestParam("id") final Long id) {
+    public ResponseEntity<?> delete(@RequestParam("id") final Long id){
         try {
-            produtoService.delete(id);
-            return ResponseEntity.ok("Registro deletado com sucesso");
-        } catch (EntityNotFoundException e) {
-            return ResponseEntity.badRequest().body(e.getCause());
+            service.delete(id);
+            return ResponseEntity.ok("Sucesso ao deletar Registro!");
+        }catch(Exception e){
+            return ResponseEntity.badRequest().body("Error : " + e.getMessage());
         }
     }
-
 
 
 }
