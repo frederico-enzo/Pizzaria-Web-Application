d97ccad87c5f3097190cd534c7c9b2020afb77ee
Frederico Enzo <portifolio.frederico@gmail.com>
2023-10-10 17:51:59 -0300

    Alguma coisa esta errado nao acesseitava o bootstrap tiver que refazer o projeto

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -2,30 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { IndexComponent } from './layout/index/index.component';
-import { HeaderComponent } from './layout/header/header.component';
-import { FooterComponent } from './layout/footer/footer.component';
-import { LoginComponent } from './system/login/login.component';
-import { SignUpComponent } from './system/sign-up/sign-up.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { FormsModule } from '@angular/forms';
-import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    IndexComponent,
-    HeaderComponent,
-    FooterComponent,
-    LoginComponent,
-    SignUpComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    NgbModule,
-    FormsModule,
-    AppRoutingModule,
-    
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
