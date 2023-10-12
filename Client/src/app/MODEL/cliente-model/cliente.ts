97ad9dcaad163258b073b0fd86dc0a2d7502b1b3
Frederico Enzo <portifolio.frederico@gmail.com>
2023-10-12 13:58:14 -0300

    Model Endereco (ok)

--- Client/src/app/MODEL/cliente-model/cliente.ts
+++ Client/src/app/MODEL/cliente-model/cliente.ts
@@ -1,7 +1,10 @@
+import { Endereco } from "../endereco-model/endereco";
+
 export class Cliente {
     id!: Number;
     nome!: String;
     telefone!: String;
     email!: String;
     senha!: String;
+    endereco!: Endereco;
 }
