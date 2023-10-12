66291c3b1b0fdbb2f666ed5dd0219c3fa86bb91c
Frederico Enzo <portifolio.frederico@gmail.com>
2023-10-12 16:15:49 -0300

    Iniciando os components com o login e layout

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LoginComponent } from './system/login/login.component';
+import { HeaderComponent } from './COMPONENTS/layout/header/header.component';
+import { FooterComponent } from './COMPONENTS/layout/footer/footer.component';
+import { IndexComponent } from './COMPONENTS/layout/index/index.component';
+import { SignUpComponent } from './COMPONENTS/system/sign-up/sign-up.component';
+import { SignInComponent } from './COMPONENTS/system/sign-in/sign-in.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HeaderComponent,
+    FooterComponent,
+    IndexComponent,
+    SignUpComponent,
+    SignInComponent
   ],
   imports: [
     BrowserModule,
